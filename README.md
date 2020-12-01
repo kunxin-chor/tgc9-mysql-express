a3483cec90582f16fecbbd179abaf6953c7bca46
kunxin-chor <chorkunxin@yahoo.com>
2020-12-01 04:32:17 +0000

    Added C for M:M example

--- README.md
+++ README.md
@@ -76,3 +76,76 @@ and see if it can send back the results
 6. Create the hbs file and extend (but don't do the `#each` yet)
 
 7. Create the final output using `#each`
+
+# PSUEDO CODE FOR CRUD
+
+## THE GENERIC FLOW
+
+0. Look at the database and figure whether we are dealing
+   with a strong entity, a weak entity using 1:M relationship,
+   or a weak entity using M:N relationship
+1. A route to recieve the request from the Client
+   1. We need the URL (which URL does the cliebt goes to activate)
+   2. The view function (`aysnc (req,res)`)
+   3. Response -- .hbs (`res.render`) or a json response (`res.send`)?
+2. A query that will the route will send to the DB
+    1. 
+3. A result that is sent from DB
+4. A template (response) that will be sent back to the Client
+   1. What do pass from the view function into the hbs file
+
+This is also known the 3-Tier architecture
+
+## READ
+1. We decide what exactly we want to do retrieve from the database
+2. Formulate the query for that, and we test using whatever storage system
+(json files, API endpoints, MySQL, Mongo etc.)
+3. Then we create a route 
+4. In the route, execute the query and store the results
+5. Send the results to the template file
+```
+              4. response
+    + -------------------+
+    |                    ^
+    |      1. request    |        2. query       (json file, API etc.)
+<Client> -----------> <server>  -------------->  <DB> 
+                        ^                          |
+                        |                          |
+                        +--------------------------+
+                                  3. results
+```
+
+## Create
+1. The route must display a form for the user to fill in
+   * addenum: just need some way for the user to provide data to the 
+              route
+              1. fill in a form
+              2. upload a file
+              3. through the URL
+              4. through a JSON sent via axios
+
+2. write the query to inser the data into storage (json file, API endpoint, database)
+3. notify the user that it's done / redirect back to the listing page (depending on requirement)
+
+For our dynamic web app, we need 2 routes
+
+1. To display the form
+   * If the field is a foreign key, we must use a select dropdown
+   ** this means, we need a query to fetch all the possible selection for the
+      select dropdown
+   * if the field is a foreign key to a pivot table (meaning, it's a M:M), then
+     we use the `<select>` but we must allow the user to select multiple
+    * when creating the form, suggest the `name` attribute for each input
+      to be the same as the columns in the table
+    * use `<select>` to allow the user to choose from the possible foreign key
+    * test the form
+        * make sure the form shows up in the first place
+        * make sure the when we submit is via POST
+        * make sure all the fields are filled in correctly
+        * make sure there are no missing fields
+2. Process the form
+    * first test: make sure to echo back (use `res.send` or `console.log`) and that there is no missing fields
+    * extract out the fields that we want to insert into the database
+    * write a test query
+    * put the test query into the code and replace all the fixed values with question mark
+    * use `connection.execute` with the query as the first argumnet, and an array of values to fill in as the second
--- index.js
+++ index.js
@@ -1,197 +1,288 @@
-const express = require('express');
-const hbs = require('hbs');
-const wax = require('wax-on');
-const fs = require('fs'); // import in the file system
-const mysql = require('mysql2/promise')
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+const fs = require("fs"); // import in the file system
+const mysql = require("mysql2/promise");
 
 let app = express();
 // set which view engine to use
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // set where to find the static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // setup wax on for template inheritance
 wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts');
+wax.setLayoutPath("./views/layouts");
 
 // setup forms
-app.use(express.urlencoded({
-    extended:false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-const helpers = require('handlebars-helpers')({
-  handlebars: hbs.handlebars
+const helpers = require("handlebars-helpers")({
+  handlebars: hbs.handlebars,
 });
 
 async function main() {
-    // creating the connection is an asynchronous procedure,
-    // so we must use await 
-    const connection = await mysql.createConnection({
-        'host':'localhost',
-        'user':'root',
-        'database': 'sakila'
-    })
+  // creating the connection is an asynchronous procedure,
+  // so we must use await
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "sakila",
+  });
 
-    app.get('/', async (req,res)=>{
-        // connection.execute is async
-        let [actors, fields] = await connection.execute('select * from actor');
-        res.render('actors.hbs', {
-            'actors': actors
-        })
-    })
-
-    app.get('/actor/create', async(req,res)=>{
-        res.render('create_actor.hbs');
-    })
-
-    app.post('/actor/create', async(req,res)=>{
-        let firstName = req.body.first_name;
-        let lastName = req.body.last_name;
-
-        await connection.execute(`insert into actor (first_name, last_name)
-                                  values (?, ?)`, [firstName, lastName]);
-
-        res.redirect('/');
-
+  app.get("/", async (req, res) => {
+    // connection.execute is async
+    let [actors, fields] = await connection.execute("select * from actor");
+    res.render("actors.hbs", {
+      actors: actors,
     });
+  });
 
-    // the :actor_id placeholder is for the url to specify which actor we are editing
-    app.get('/actor/:actor_id/update', async (req,res)=>{
-        let [actors] = await connection.execute(`select * from actor where actor_id = ?`,
-         [req.params.actor_id]);
-        let theActor = actors[0];
- 
-        res.render('edit_actor.hbs',{
-            'actor': theActor
-        });
-    })  
+  app.get("/actor/create", async (req, res) => {
+    res.render("create_actor.hbs");
+  });
 
-    app.post('/actor/:actor_id/update', async (req,res)=>{
-       let firstName = req.body.first_name;
-       let lastName = req.body.last_name;
-       let actorId = req.params.actor_id;
+  app.post("/actor/create", async (req, res) => {
+    let firstName = req.body.first_name;
+    let lastName = req.body.last_name;
 
-       await connection.execute(`update actor set first_name = ?, last_name=?
-                                WHERE actor_id = ?`, [firstName, lastName, actorId])
+    await connection.execute(
+      `insert into actor (first_name, last_name)
+                                  values (?, ?)`,
+      [firstName, lastName]
+    );
 
-       res.redirect('/');
+    res.redirect("/");
+  });
 
-    })
+  // the :actor_id placeholder is for the url to specify which actor we are editing
+  app.get("/actor/:actor_id/update", async (req, res) => {
+    let [
+      actors,
+    ] = await connection.execute(`select * from actor where actor_id = ?`, [
+      req.params.actor_id,
+    ]);
+    let theActor = actors[0];
 
-    app.get('/actor/:actor_id/delete', async(req,res)=>{
-        let theActorToDelete = req.params.actor_id;
-        // BUT we will always get an array back from connection.execute EVEN if there is only one result
-        let [actors] = await connection.execute(`select * from actor where actor_id= ?`, [theActorToDelete])
-        let theActor = actors[0];
-        // res.send(theActor);
+    res.render("edit_actor.hbs", {
+      actor: theActor,
+    });
+  });
 
-        res.render('delete_actor.hbs',{
-            'actor': theActor
-        })
-    })
+  app.post("/actor/:actor_id/update", async (req, res) => {
+    let firstName = req.body.first_name;
+    let lastName = req.body.last_name;
+    let actorId = req.params.actor_id;
 
-    app.post('/actor/:actor_id/delete', async(req,res)=>{
-        let actor_id = req.params.actor_id;
-        await connection.execute(`delete from actor where actor_id = ?`, [actor_id]);
-        res.redirect('/')
-    })
+    await connection.execute(
+      `update actor set first_name = ?, last_name=?
+                                WHERE actor_id = ?`,
+      [firstName, lastName, actorId]
+    );
 
-    app.get('/languages', async (req, res) =>{
-        let [languages] = await connection.execute('select * from language');
+    res.redirect("/");
+  });
 
-        res.render('languages.hbs', {
-            'lang': languages
-        })
-    })
+  app.get("/actor/:actor_id/delete", async (req, res) => {
+    let theActorToDelete = req.params.actor_id;
+    // BUT we will always get an array back from connection.execute EVEN if there is only one result
+    let [
+      actors,
+    ] = await connection.execute(`select * from actor where actor_id= ?`, [
+      theActorToDelete,
+    ]);
+    let theActor = actors[0];
+    // res.send(theActor);
 
-    app.get('/countries', async (req, res)=>{
-        let [countries] = await connection.execute('select * from country');
-        res.render('countries.hbs',{
-            'countries': countries
-        })
-    })
+    res.render("delete_actor.hbs", {
+      actor: theActor,
+    });
+  });
 
-    app.get('/cities', async(req,res)=>{
+  app.post("/actor/:actor_id/delete", async (req, res) => {
+    let actor_id = req.params.actor_id;
+    await connection.execute(`delete from actor where actor_id = ?`, [
+      actor_id,
+    ]);
+    res.redirect("/");
+  });
 
-        // when we do a R for a weak entity that is 1:M relationship
-        // -- to get the full details, we have to do a join
-        let [cities] = await connection.execute(`
+  app.get("/languages", async (req, res) => {
+    let [languages] = await connection.execute("select * from language");
+
+    res.render("languages.hbs", {
+      lang: languages,
+    });
+  });
+
+  app.get("/countries", async (req, res) => {
+    let [countries] = await connection.execute("select * from country");
+    res.render("countries.hbs", {
+      countries: countries,
+    });
+  });
+
+  app.get("/cities", async (req, res) => {
+    // when we do a R for a weak entity that is 1:M relationship
+    // -- to get the full details, we have to do a join
+    let [cities] = await connection.execute(`
             select * from city join country on
-            city.country_id = country.country_id`
-        );
-        // res.send(cities);
-        res.render('cities.hbs', {
-            'cities': cities
-        })
+            city.country_id = country.country_id`);
+    // res.send(cities);
+    res.render("cities.hbs", {
+      cities: cities,
+    });
+  });
 
-    })
+  app.get("/city/create", async (req, res) => {
+    let [countries] = await connection.execute("select * from country");
+    res.render("create_city", {
+      countries: countries,
+    });
+  });
 
-    app.get('/city/create', async (req,res)=>{
-        let [countries] = await connection.execute('select * from country');
-        res.render('create_city',{
-            'countries': countries
-        })
-    })
+  app.post("/city/create", async (req, res) => {
+    // await connection.execute(`
+    //     insert into city (city, country_id)
+    //     VALUES (?, ?)`, [
+    //         req.body.city,
+    //         req.body.country_id
+    //     ])
+    let city = req.body.city;
+    let country_id = req.body.country_id;
+    await connection.execute(
+      `insert into city (city, country_id)
+                VALUES (?, ?)`,
+      [city, country_id]
+    );
+    res.redirect("/cities");
+  });
 
-    app.post('/city/create', async(req,res)=>{
-        // await connection.execute(`
-        //     insert into city (city, country_id)
-        //     VALUES (?, ?)`, [
-        //         req.body.city,
-        //         req.body.country_id
-        //     ])
-            let city = req.body.city;
-            let country_id = req.body.country_id;
-            await connection.execute(`insert into city (city, country_id)
-                VALUES (?, ?)`, [city, country_id]);
-            res.redirect('/cities')
-        })
+  app.get("/city/:city_id/update", async (req, res) => {
+    let [countries] = await connection.execute("select * from country");
+    let wantedCityID = req.params.city_id;
+    let [
+      cities,
+    ] = await connection.execute(`select * from city where city_id = ?`, [
+      wantedCityID,
+    ]);
+    let theCity = cities[0];
+    res.render("edit_city", {
+      countries: countries,
+      city: theCity,
+    });
+  });
 
-    app.get('/city/:city_id/update', async (req,res)=>{
-        let [countries] = await connection.execute('select * from country');
-        let wantedCityID = req.params.city_id;
-        let [cities] = await connection.execute(`select * from city where city_id = ?`, [wantedCityID]);
-        let theCity = cities[0];
-        res.render('edit_city', {
-            'countries': countries,
-            'city': theCity
-        })
-    })
-
-    app.post('/city/:city_id/update', async(req,res)=>{
-        let city = req.body.city;
-        let country_id = req.body.country_id;
-        await connection.execute(`
+  app.post("/city/:city_id/update", async (req, res) => {
+    let city = req.body.city;
+    let country_id = req.body.country_id;
+    await connection.execute(
+      `
             update city set city= ?, country_id = ?
-            WHERE city_id= ?`, [city, country_id, req.params.city_id])
-        res.redirect('/cities');
-    })
+            WHERE city_id= ?`,
+      [city, country_id, req.params.city_id]
+    );
+    res.redirect("/cities");
+  });
 
-    app.get('/stores', async (req,res)=>{
-        /*
+  app.get("/stores", async (req, res) => {
+    /*
             select store.store_id, address, address2, first_name, last_name from store join staff
             on store.manager_staff_id = staff.staff_id
             join address ON
             store.address_id = address.address_id
         */
-        let [stores] = await connection.execute(`
+    let [stores] = await connection.execute(`
             select store.store_id, address, address2, first_name, last_name from store join staff
             on store.manager_staff_id = staff.staff_id
             join address ON
             store.address_id = address.address_id
         `);
 
-        // res.send(stores);
-        res.render('stores.hbs',{
-            'stores': stores
-        })
-    })
+    // res.send(stores);
+    res.render("stores.hbs", {
+      stores: stores,
+    });
+  });
 
-}
+  app.get("/films", async (req, res) => {
+    /*
+        Sample query:
+            select * from film
+        */
+    let [results] = await connection.execute("select * from film");
+
+    res.render("films", {
+      films: results,
+    });
+  });
+
+  app.get("/films/create", async (req, res) => {
+    let [languages] = await connection.execute("select * from language");
+    let [actors] = await connection.execute('select * from actor');
+    res.render("create_film", {
+      languages: languages,
+      actors: actors
+    });
+  });
+
+  app.post("/films/create", async (req, res) => {
+    //   res.send(req.body);
+    // traditional method
+    // let title = req.body.title;
+    // let description = req.body.description;
+    // let release_year = req.body.release_year;
+    // let language_id = req.body.language_id;
+    // let rental_duration = req.body.language_id;
+    // let replacement_cost = req.body.replacement_cost;
+
+    // OR use the modern method (object destructuring)
+    let {
+      title,
+      description,
+      release_year,
+      language_id,
+      rental_duration,
+      replacement_cost,
+      actor_id
+    } = req.body;
+
+    let [results] = await connection.execute(
+      `
+        insert into film 
+        ( title, description, release_year, language_id, rental_duration, replacement_cost)
+        values (?, ?, ?, ?, ?, ?)`,
+      [
+        title,
+        description,
+        release_year,
+        language_id,
+        rental_duration,
+        replacement_cost,
+      ]
+    );
+
+    // get the film_id of the film we just created
+    let newFilmId = results.insertId;
+
+    for (let eachActor of actor_id) {
+        connection.execute(`insert into film_actor (actor_id, film_id)
+            values (?, ?)`, [
+                eachActor, newFilmId
+            ])
+    }
+
+    res.redirect('/films');
+  });
+} //  end mains
 
 main();
 
-app.listen(3000, ()=>{
- console.log("Server started");
+app.listen(3000, () => {
+  console.log("Server started");
 });
